15e20b9a5d4ca2d514311bc17af6c3253ed8bba9
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-10-14 21:52:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const router =require('./src/routes/api');
 const app= new express();
 const bodyParser =require('body-parser');
 const path= require('path');
-const path2= require('path2');
 
 
 // Security Middleware Lib Import
