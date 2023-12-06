e276b0e47689dc35a0e8a19f9dbde748fa12dd63
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-06 19:20:43 +0600

    model changed, readme added

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://documenter.getpostman.com/view/28939375/2s9YR3dbJG
--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -1,20 +1,20 @@
-const moongose = require('mongoose');
+const moongose = require("mongoose");
 
-const DataSchema = moongose.Schema({
-        ProductName: {type: String, required: true},
-        ProductCode: {type: String, unique: true, required: true},
-        Img: {type: String},
-        UnitPrice: {type: Number},
-        Qty: {type: Number, default: 0},
-        TotalPrice: {type: Number, required: true},
-        CreatedDate: {type: Date, default: Date.now()}
-    },
-    {
-        versionKey: false,
-        timestamps: true
-    }
-)
+const DataSchema = moongose.Schema(
+  {
+    ProductName: { type: String, required: true },
+    ProductCode: { type: String, unique: true, required: true },
+    Img: { type: String, required: true },
+    UnitPrice: { type: Number, required: true },
+    Qty: { type: Number, required: true, default: 0 },
+    TotalPrice: { type: Number, required: true },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
-const ProductsModel = moongose.model('products', DataSchema); //Here products is database's collection name
-module.exports = ProductsModel
-
+const ProductsModel = moongose.model("products", DataSchema); //Here products is database's collection name
+module.exports = ProductsModel;
